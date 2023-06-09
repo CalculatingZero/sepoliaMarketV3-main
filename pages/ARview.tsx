ce3783f0e64decf39349564f11977a2710c0639a
CalculatingZero <20590197+CalculatingZero@users.noreply.github.com>
2023-06-08 23:25:29 -0700

    Update ARview.tsx

--- pages/ARview.tsx
+++ pages/ARview.tsx
@@ -9,37 +9,72 @@ import Container from "../components/Container/Container";
 import { ComponentProps } from 'react';
 import { useCursor} from '@react-three/drei';
 
-function Image() {
-    var img = "/slideshow/joffee.png";
-    const texture = useLoader(THREE.TextureLoader, img)
-
+function Box(props: ThreeElements['mesh']) {
+  const ref = useRef<THREE.Mesh>(null!)
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
   return (
-
-    <mesh>
-      <planeGeometry attach="geometry" args={[10, 10]} />
-      <meshBasicMaterial attach="material" map={texture} />
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[3, 3, 3]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
-  
   )
 }
 
 export default function ARview() {
-  return (
+  return(
+    <>
+   
+    <ARButton />
+ 
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-4.2, 0, 0]} />
+        <Box position={[4.2, 0, 0]} />
+      </Canvas>
 
-    <Container maxWidth="sm">
-      <Navbar/>
+  </>
+)
+  }
+// function Image() {
+//     var img = "/slideshow/joffee.png";
+//     const texture = useLoader(THREE.TextureLoader, img)
+
+//   return (
+
+//     <mesh>
+//       <planeGeometry attach="geometry" args={[10, 10]} />
+//       <meshBasicMaterial attach="material" map={texture} />
+//     </mesh>
+  
+//   )
+// }
+
+// export default function ARview() {
+//   return (
+
+//     <Container maxWidth="sm">
+//       <Navbar/>
      
       
-        <ARButton />
-        <Suspense fallback={null}>
-          <Canvas>
-              <XR>  
-                <Image />
-              </XR>
-          </Canvas>
-        </Suspense>
-    </Container>
+//         <ARButton />
+//         <Suspense fallback={null}>
+//           <Canvas>
+//               <XR>  
+//                 <Image />
+//               </XR>
+//           </Canvas>
+//         </Suspense>
+//     </Container>
 
 
-  )
-}
+//   )
+// }
